937312aeed82bf6d669e772ac0f1111cb2f425f1
Keenan Pautler <kpautler@henselphelps.com>
2019-04-10 16:44:59 -0600

    Create Navigation Bar and Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+function Header() {
+  const style = {
+    jumbotron: {
+      'text-align': 'center',
+    },
+  };
+  return (
+    <Jumbotron style={style.jumbotron}>
+      <h1>Clicker Game</h1>
+      <br />
+      <h2>
+        Click on an image to earn points, but you can't click on an image more
+        than once
+      </h2>
+    </Jumbotron>
+  );
+}
+
+export default Header;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+class Navigation extends React.Component {
+  render() {
+    const style = {
+      header: {
+        'box-shadow':
+          '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+      },
+      jumbotron: {
+        'text-align': 'center',
+      },
+    };
+    return (
+      <Navbar bg="dark" variant="dark" style={style.header}>
+        <Nav>
+          <Navbar.Brand href="/">
+            <h1>Clicker Game</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Nav>
+        <Nav className="m-auto">
+          <Navbar.Text>
+            <h1>Click an image to begin!</h1>
+          </Navbar.Text>
+        </Nav>
+        <Nav>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <h1>Score:</h1>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Nav>
+      </Navbar>
+    );
+  }
+}
+
+export default Navigation;
